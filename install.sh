65b1177f861125c4b5ed3bfb548833e201c65512
gucheng6 <41187062+gucheng6@users.noreply.github.com>
2018-07-16 16:24:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ read -p "Please input your web password：" webpasswd
 
 
 #Install SSR (Powered By Teddysun : https://shadowsocks.be/9.html)
-wget -N --no-check-certificate https://raw.githubusercontent.com/FunctionClub/shadowsocks_install/master/shadowsocksR.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh
 chmod +x shadowsocksR.sh
 bash shadowsocksR.sh
 rm -rf shadowsocksR.sh
